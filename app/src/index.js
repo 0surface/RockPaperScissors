5f5e2e061f9ac48e2bb3eecc4f044133e118f542
0surface <tms.0xvoid@gmail.com>
2020-11-19 19:37:43 +0000

    inital unbox, minimum setup

---
+++ app/src/index.js
@@ -0,0 +1,68 @@
+const Web3 = require("web3");
+const truffleContract = require("truffle-contract");
+const $ = require("jquery");
+const rockPaperScissorsJson = require("../../build/contracts/RockPaperScissors.json");
+
+const App = {
+  web3: null,
+  wallets: [],
+  rockPaperScissors: truffleContract(rockPaperScissorsJson),
+
+  start: async function () {
+    const { web3, $ } = this;
+    try {
+      this.rockPaperScissors.setProvider(web3.currentProvider);
+      await this.setUpApp();
+    } catch (error) {
+      console.log(error);
+      console.error("Could not connect to contract or chain.");
+    }
+  },
+
+  showContractBalance: async function () {
+    const deployed = await this.rockPaperScissors.deployed();
+    const balanceInWei = await this.web3.eth.getBalance(deployed.address);
+    const balanceElement = document.getElementById("contractBalance");
+    const balanceInEther = this.web3.utils.fromWei(balanceInWei, "ether");
+    balanceElement.innerHTML = `${parseFloat(balanceInEther).toFixed(4)} ETH`;
+  },
+
+  setUpApp: async function () {
+    const { web3 } = this;
+
+    const labels = ["Deployer", "Accts 2", "Accts 3", "Accts 4", "Accts 5"];
+
+    web3.eth
+      .getAccounts()
+      .then((accounts) => {
+        if (accounts.length == 0) {
+          throw new Error("No accounts with which to transact");
+        }
+        return accounts;
+      })
+      .then((accountList) => {
+        for (i = 0; i < 10; i++) {
+          let address = accountList[i];
+          let label = labels[i];
+          this.wallets.push({ i, address, label });
+        }
+      })
+      .catch(console.error);
+
+    await this.showContractBalance();
+  },
+};
+
+window.App = App;
+
+window.addEventListener("load", function () {
+  if (window.ethereum) {
+    App.web3 = new Web3(window.ethereum);
+    window.ethereum.enable(); // get permission to access accounts
+  } else {
+    //Fall back local provider
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  }
+
+  App.start();
+});
